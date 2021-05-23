0fecb53c383bb05ad65b0832a075c2237f1a2d62
devopsengineerDan <devopsengineerdan@gmail.com>
2021-05-23 13:26:44 +0300

    engineer

--- waweru/settings.py
+++ waweru/settings.py
@@ -71,8 +71,7 @@ WSGI_APPLICATION = 'waweru.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -82,7 +81,17 @@ DATABASES = {
         
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': ''
+        
+    }
+}
 
 # if config('MODE')=="dev":
 #     DATABASES = {
